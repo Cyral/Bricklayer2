154276dc562d6c40221f55f28e5284131995b7d5
Cyral <cyral@pyratron.com>
2015-06-06 20:09:24 -0400

    Basic classes for tiles/world/player

---
+++ Source/Core/Common/World/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricklayer.Core.Common.World
+{
+    /// <summary>
+    /// Represents a layer for tiles to be drawn on and interacted with.
+    /// </summary>
+    public enum Layer : byte
+    {
+        Foreground,
+        Background,
+        All,
+    }
+}
