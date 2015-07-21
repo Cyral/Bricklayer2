7a66e7696d20c0b393a7adf2b3f75e8a51c5c0d3
Cyral <cyral@pyratron.com>
2015-07-20 20:45:59 -0400

    Chat improvements

--- Source/Core/Client/Interface/Screens/GameScreen.cs
+++ Source/Core/Client/Interface/Screens/GameScreen.cs
@@ -1,4 +1,5 @@
-﻿using Bricklayer.Core.Client.Interface.Controls;
+﻿using System.Text;
+using Bricklayer.Core.Client.Interface.Controls;
 using Bricklayer.Core.Client.World;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -37,9 +38,10 @@ namespace Bricklayer.Core.Client.Interface.Screens
             //Chat input box
             txtChat = new TextBox(Manager);
             txtChat.Init();
-            txtChat.Left = 30;
-            txtChat.Top = sbStats.Top - 50;
-            txtChat.Width = (int)(Manager.TargetWidth * 0.4f);
+            txtChat.Left = 8;
+            txtChat.DrawFormattedText = false;
+            txtChat.Bottom = sbStats.Top - 8;
+            txtChat.Width = (int)(Manager.TargetWidth * .4f) - 16; //Remove 16 to align due to invisible scrollbar
             txtChat.Visible = false;
             txtChat.Passive = true;
             Window.Add(txtChat);
@@ -47,63 +49,86 @@ namespace Bricklayer.Core.Client.Interface.Screens
             lstChats = new ControlList<ChatDataControl>(Manager)
             {
                 Left = txtChat.Left,
-                Width = txtChat.Width,
-                Height = (int)(Manager.TargetHeight * 0.25f),
+                Width = txtChat.Width + 16,
+                Height = (int)(Manager.TargetHeight * .25f),
             };
             lstChats.Init();
             lstChats.Color = Color.Transparent;
             lstChats.HideSelection = true;
             lstChats.Passive = true;
+            lstChats.HideScrollbars = true;
             lstChats.Top = txtChat.Top - lstChats.Height;
             Window.Add(lstChats);
-            // Hackish way to get chats to start at the bottom
-            for (var i = 0; i < (Manager.TargetHeight * 0.25f) / 18; i++)
-            {
-                lstChats.Items.Add(new ChatDataControl("", Manager, lstChats));
-            }
+            for (var i = 0; i < lstChats.Height / 16; i++) //Hack to make chat start at bottom (easier than modifying rendering code)
+                lstChats.Items.Add(new ChatDataControl(string.Empty, Manager, lstChats));
         }
 
         public override void Update(GameTime gameTime)
         {
             lblStats.Text = "FPS: " + Window.FPS;
 
-            if (Client.Input.WasKeyPressed(Keys.T) && !txtChat.Visible) // Open chat
+            HandleInput();
+
+            base.Update(gameTime);
+        }
+
+        private void HandleInput()
+        {
+            if (Client.Input.IsKeyPressed(Keys.T) && !txtChat.Visible) // Open chat
             {
                 txtChat.Visible = true;
                 txtChat.Passive = false;
                 txtChat.Focused = true;
-
                 lstChats.Items.ForEach(x => ((ChatDataControl)x).Show());
             }
-            else if (Client.Input.WasKeyPressed(Keys.Enter) && txtChat.Visible)
+            else if ((Client.Input.IsKeyPressed(Keys.Enter) && txtChat.Visible) || Client.Input.IsKeyPressed(Keys.Escape))
             {
-                if (!string.IsNullOrWhiteSpace(txtChat.Text)) // If there's characters in chatbox, send chat
+                // If there's characters in chatbox, send chat
+                // Cancel out of chat if player clicks escape
+                if (!string.IsNullOrWhiteSpace(txtChat.Text) && !Client.Input.IsKeyPressed(Keys.Escape))
                 {
-                    lstChats.Items.Add(new ChatDataControl(txtChat.Text, Manager, lstChats));
+                    AddChat(txtChat.Text, Manager, lstChats);
                     lstChats.ScrollTo(lstChats.Items.Count);
-                    txtChat.Text = "";
-                    txtChat.Visible = false;
-                    txtChat.Passive = true;
-                    txtChat.Focused = false;
-                    lstChats.Items.ForEach(x => ((ChatDataControl)x).UnShow());
+                    txtChat.Text = string.Empty;
                 }
-                else // If nothing is typed and player clicked enter, close out of chat
-                {
-                    txtChat.Visible = false;
-                    txtChat.Passive = true;
-                    txtChat.Focused = false;
-                    lstChats.Items.ForEach(x => ((ChatDataControl)x).UnShow());
-                }
-            }
-            // Cancel out of chat if player clicks escape
-            else if (Client.Input.WasKeyPressed(Keys.Escape) && txtChat.Visible)
-            {
+                // If nothing is typed and player clicked enter, close out of chat
                 txtChat.Visible = false;
                 txtChat.Passive = true;
                 txtChat.Focused = false;
-                lstChats.Items.ForEach(x => ((ChatDataControl)x).UnShow());
+                lstChats.Items.ForEach(x => ((ChatDataControl)x).Hide());
             }
-            base.Update(gameTime);
+        }
+
+        private void AddChat(string text, Manager manager, ControlList<ChatDataControl> controlList)
+        {
+            var lines = WrapText(text, lstChats.Width - 8, FontSize.Default9).Split('\n');
+            foreach (var line in lines)
+                lstChats.Items.Add(new ChatDataControl(line, Manager, lstChats));
+        }
+
+        private string WrapText(string text, float maxLineWidth, FontSize fontsize)
+        {
+            var spriteFont = Manager.Skin.Fonts[fontsize.ToString()].Resource;
+            var words = text.Split(' ');
+            var sb = new StringBuilder();
+            var lineWidth = 0f;
+            var spaceWidth = spriteFont.MeasureString(" ").X;
+            foreach (var word in words)
+            {
+                var size = spriteFont.MeasureRichString(word, Manager);
+
+                if (lineWidth + size.X < maxLineWidth)
+                {
+                    sb.Append(word + " ");
+                    lineWidth += size.X + spaceWidth;
+                }
+                else
+                {
+                    sb.Append("\n" + word + " ");
+                    lineWidth = size.X + spaceWidth;
+                }
+            }
+            return sb.ToString();
         }
 
         /// <summary>
