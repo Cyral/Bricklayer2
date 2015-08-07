51d5cf881cfab5a6089679e0b8aed2502295d678
Cyral <cyral@pyratron.com>
2015-08-07 12:45:15 -0400

    Refactor inventory code

--- Source/Core/Client/Interface/Screens/GameScreen.cs
+++ Source/Core/Client/Interface/Screens/GameScreen.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using Bricklayer.Core.Client.Interface.Controls;
 using Bricklayer.Core.Client.World;
@@ -15,19 +13,17 @@ namespace Bricklayer.Core.Client.Interface.Screens
     {
         protected internal override GameState State => GameState.Game;
         internal Level Level => Client.Level;
-        private StatusBar sbStats;
+        private Button[] btnInventory;
+        private bool inventorySizeChanging;
+        private int inventoryWidthChange;
+        private bool isInventoryOpen;
         private Label lblStats;
         private ControlList<ChatDataControl> lstChats;
         private ControlList<PlayerListDataControl> lstPlayers;
-        private TextBox txtChat;
-
+        private int normalInventoryHeight = 64, normalInventoryWidth;
         private Panel pnInventory;
-        private Button[] btnInventory;
-        private bool OpenInventory = false;
-        private int NormWidth;
-        private int NormHeight;
-        private int WidthChange;
-        private bool Changing = false;
+        private StatusBar sbStats;
+        private TextBox txtChat;
 
         /// <summary>
         /// Setup the game UI.
@@ -42,7 +38,7 @@ namespace Bricklayer.Core.Client.Interface.Screens
             sbStats.Left = 0;
             sbStats.Width = Manager.ScreenWidth;
 
-            lblStats = new Label(Manager) { Top = 4, Left = 8, Width = Manager.ScreenWidth - 16 };
+            lblStats = new Label(Manager) {Top = 4, Left = 8, Width = Manager.ScreenWidth - 16};
             lblStats.Init();
 
             sbStats.Add(lblStats);
@@ -52,19 +48,22 @@ namespace Bricklayer.Core.Client.Interface.Screens
             btnInventory = new Button[11];
 
             // Inventory
+            normalInventoryWidth = (48*btnInventory.Length) + ((btnInventory.Length + 1)*10);
             pnInventory = new Panel(Manager)
             {
                 // (48*9) + (10*10)
-                Width = (48*btnInventory.Length)+((btnInventory.Length+1)* 10), // Make room for 11 slots and 10 space inbetween
-                Height = 50
+                // Make room for 11 slots and 10 space in between
+                Width = normalInventoryWidth,
+                Height = normalInventoryHeight
             };
             pnInventory.Left = Manager.TargetWidth/2 - (pnInventory.Width/2);
             pnInventory.Init();
-            Window.Add(pnInventory);
-            NormHeight = 50;
-            NormWidth = (48 * btnInventory.Length) + ((btnInventory.Length + 1) * 10);
 
-            for (int i = 0; i < btnInventory.Count(); i++)
+            Window.Add(pnInventory);
+            normalInventoryHeight = 50;
+            normalInventoryWidth = (48*btnInventory.Length) + ((btnInventory.Length + 1)*10);
+
+            for (var i = 0; i < btnInventory.Count(); i++)
             {
                 btnInventory[i] = new Button(Manager)
                 {
@@ -84,7 +83,7 @@ namespace Bricklayer.Core.Client.Interface.Screens
             txtChat.Left = 8;
             txtChat.DrawFormattedText = false;
             txtChat.Bottom = sbStats.Top - 8;
-            txtChat.Width = (int)(Manager.TargetWidth * .4f) - 16; // Remove 16 to align due to invisible scrollbar
+            txtChat.Width = (int) (Manager.TargetWidth*.4f) - 16; // Remove 16 to align due to invisible scrollbar
             txtChat.Visible = false;
             txtChat.Passive = true;
             Window.Add(txtChat);
@@ -93,7 +92,7 @@ namespace Bricklayer.Core.Client.Interface.Screens
             {
                 Left = txtChat.Left,
                 Width = txtChat.Width + 16,
-                Height = (int)(Manager.TargetHeight * .25f),
+                Height = (int) (Manager.TargetHeight*.25f)
             };
             lstChats.Init();
             lstChats.Color = Color.Transparent;
@@ -106,11 +105,11 @@ namespace Bricklayer.Core.Client.Interface.Screens
             lstPlayers = new ControlList<PlayerListDataControl>(Manager)
             {
                 Width = 256,
-                Top = 256,
+                Top = 256
             };
             lstPlayers.Init();
             lstPlayers.HideSelection = true;
-            lstPlayers.Left = Manager.TargetWidth/2 - (lstPlayers.Width / 2);
+            lstPlayers.Left = Manager.TargetWidth/2 - (lstPlayers.Width/2);
             lstPlayers.Passive = true;
             lstPlayers.HideScrollbars = true;
             lstPlayers.Visible = false;
@@ -121,135 +120,111 @@ namespace Bricklayer.Core.Client.Interface.Screens
 
 
             // Listen for later player joins
-            Client.Events.Network.Game.PlayerJoinReceived.AddHandler(args =>
-            {
-                lstPlayers.Items.Add(new PlayerListDataControl(args.Player, Manager, lstPlayers));
-            });
+            Client.Events.Network.Game.PlayerJoinReceived.AddHandler(
+                args => { lstPlayers.Items.Add(new PlayerListDataControl(args.Player, Manager, lstPlayers)); });
 
             // Listen for ping updates for players
             Client.Events.Network.Game.PingUpdateReceived.AddHandler(args =>
             {
                 foreach (var ping in args.Pings)
                 {
-                    var control = (PlayerListDataControl)lstPlayers.Items.FirstOrDefault(i => ((PlayerListDataControl)i).User.UUID == ping.Key);
+                    var control =
+                        (PlayerListDataControl)
+                            lstPlayers.Items.FirstOrDefault(i => ((PlayerListDataControl) i).User.UUID == ping.Key);
                     control?.ChangePing(ping.Value);
                 }
             });
 
 
             // Hackish way to get chats to start at the bottom
-            for (var i = 0; i < (Manager.TargetHeight * 0.25f) / 18; i++)
+            for (var i = 0; i < (Manager.TargetHeight*0.25f)/18; i++)
             {
                 lstChats.Items.Add(new ChatDataControl("", Manager, lstChats, this));
             }
 
-            Client.Events.Network.Game.ChatReceived.AddHandler(args =>
-            {
-                AddChat(args.Message, Manager, lstChats);
-            });
+            Client.Events.Network.Game.ChatReceived.AddHandler(args => { AddChat(args.Message, Manager, lstChats); });
         }
-
 
         public override void Update(GameTime gameTime)
         {
             lblStats.Text = "FPS: " + Window.FPS;
 
             HandleInput();
-            UpdateInventory();
+            UpdateInventory(gameTime);
 
 
             base.Update(gameTime);
         }
 
-        private double effect = 9; // Current movement amount for transition
-        private void UpdateInventory()
+        private void UpdateInventory(GameTime gameTime)
         {
-            if (OpenInventory) // If inventory is opening or is open
+            if (inventorySizeChanging)
             {
-                if (pnInventory.Width < WidthChange) // If the open process is still going
+                var delta = (float)gameTime.ElapsedGameTime.TotalSeconds * 20;
+                if (isInventoryOpen) // Make inventory bigger as it opens.
                 {
-                    if (effect > 1) 
-                        effect -= 0.25;
-
-                    pnInventory.Width += (int)effect;
-                    pnInventory.Height += (int)effect;
-                    pnInventory.Left = Manager.TargetWidth / 2 - (pnInventory.Width / 2); // Center the inventory box
-
-                    // Keep buttons in the center
-                    for (var i = 0; i < btnInventory.Count(); i++)
-                        btnInventory[i].Left = ((pnInventory.Width / 2) + 24 * btnInventory.Count() + 2) - (58 * i);
+                    // 25 is added/subtracted to make the lerp faster, as it will become very slow at the end.
+                    pnInventory.Width =
+                        (int) MathHelper.Lerp(pnInventory.Width, (normalInventoryWidth*1.5f) + 25, delta);
+                    pnInventory.Height =
+                        (int) MathHelper.Lerp(pnInventory.Height, (normalInventoryHeight*2) + 25, delta);
+                    if (pnInventory.Width >= normalInventoryWidth*1.5)
+                        inventorySizeChanging = false;
                 }
                 else
                 {
-                    Changing = false;
-                    effect = 9;
-                }
-            }
-            else // If inventory is closing or is closed
-            {
-                if (pnInventory.Width > NormWidth) // If the close process is still going
-                {
-                    if (effect > 1)
-                        effect -= 0.25;
-
-                    pnInventory.Width -= (int)effect;
-                    pnInventory.Height -= (int)effect;
-                    pnInventory.Left = Manager.TargetWidth / 2 - (pnInventory.Width / 2); // Center the inventory box
-
-                    // Keep buttons in the center
-                    for (var i = 0; i < btnInventory.Count(); i++)
-                        btnInventory[i].Left = ((pnInventory.Width / 2) + 24 * btnInventory.Count() + 2) - (58 * i);
-                }
-                else
-                {
-                    Changing = false;
-                    effect = 9;
+                    pnInventory.Width =
+                        (int)MathHelper.Lerp(pnInventory.Width, normalInventoryWidth - 25, delta);
+                    pnInventory.Height =
+                        (int)MathHelper.Lerp(pnInventory.Height, normalInventoryHeight, delta);
+                    if (pnInventory.Width <= normalInventoryWidth)
+                    {
+                        inventorySizeChanging = false;
+                        pnInventory.Width = normalInventoryWidth;
+                        pnInventory.Height = normalInventoryHeight;
+                    }
                 }
 
+                // Keep inventory box and controls in the center.
+                pnInventory.Left = Manager.TargetWidth/2 - (pnInventory.Width/2);
+                for (var i = 0; i < btnInventory.Length; i++)
+                    btnInventory[i].Left = ((pnInventory.Width/2) + 24*btnInventory.Length + 2) - (58*i);
             }
         }
+
         private void HandleInput()
         {
-            if (Client.Input.IsKeyPressed(Keys.T) && !txtChat.Visible) // Open chat
+            if (Client.Input.IsKeyPressed(Keys.T) && !txtChat.Visible) // Open chat.
             {
                 txtChat.Visible = true;
                 txtChat.Passive = false;
                 txtChat.Focused = true;
-                lstChats.Items.ForEach(x => ((ChatDataControl)x).Show());
+                lstChats.Items.ForEach(x => ((ChatDataControl) x).Show());
             }
-            else if ((Client.Input.IsKeyPressed(Keys.Enter) && txtChat.Visible) || Client.Input.IsKeyPressed(Keys.Escape))
+            else if ((Client.Input.IsKeyPressed(Keys.Enter) && txtChat.Visible) || Client.Input.IsKeyPressed(Keys.Escape)) // Close or send chat.
             {
-                // If there's characters in chatbox, send chat
-                // Cancel out of chat if player clicks escape
+                // If there's characters in chatbox, send chat.
+                // Cancel out of chat if player clicks escape.
                 if (!string.IsNullOrWhiteSpace(txtChat.Text) && !Client.Input.IsKeyPressed(Keys.Escape))
                 {
                     Client.Network.Send(new ChatMessage(txtChat.Text));
                     txtChat.Text = string.Empty;
                 }
-                // If nothing is typed and player clicked enter, close out of chat
+                // If nothing is typed and player clicked enter, close out of chat.
                 txtChat.Visible = false;
                 txtChat.Passive = true;
                 txtChat.Focused = false;
-                lstChats.Items.ForEach(x => ((ChatDataControl)x).Hide());
+                lstChats.Items.ForEach(x => ((ChatDataControl) x).Hide());
             }
-            else if (Client.Input.IsKeyPressed(Keys.I))
+            else if (Client.Input.IsKeyPressed(Keys.E)) // Open or close inventory.
             {
-                if (!Changing)
+                if (!inventorySizeChanging)
                 {
-                    if (!OpenInventory)
-                    {
-                        OpenInventory = true;
-                        WidthChange = pnInventory.Width + 150;
-                    }
-                    else if (OpenInventory)
-                    {
-                        OpenInventory = false;
-                    }
-                    Changing = true;
+                    isInventoryOpen = !isInventoryOpen;
+                    inventorySizeChanging = true;
                 }
             }
             lstPlayers.Visible = Client.Input.IsKeyDown(Keys.Tab);
-
         }
 
         private void AddChat(string text, Manager manager, ControlList<ChatDataControl> chatlist)
